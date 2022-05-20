cd0079697aba5877b7ea36e84651da525c833050
eudaimon <eudaimon@disroot.org>
2022-05-20 18:20:14 +0200

    First steps, incompleteç

---
+++ source/templates/variables.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+# Gray light green scheme
+
+vwindow_fg="#241f31"
+vwindow_bg="#a7a7a7"
+vtext_widget_fg="#241f31"
+vtext_widget_bg="#908f8d"
+
+vselected_fg="#000000"
+vselected_bg="#b0dd7e"
+
+vbutton_fg="#241f31"
+vbutton_bg="#bcbeb9"
+vbutton_checked_bg=$vselected_bg
+vbutton_checked_fg=$vselected_fg
+
+
+vdisabled_fg="#552222"
+vdisabled_bg="#938989"
+
+# Usually an automatic fg color on these backgrounds should check this value!
+
+vwarningc="#f57900"
+verrorc="#990000"
+vsuccessc="#73d216"
+
+
+vtitlebar_active_fg=$vselected_fg
+vtitlebar_active_bg=$vselected_bg
+
+vtitlebar_backdrop_fg=$vwindow_fg
+vtitlebar_backdrop_bg=$vwindow_bg
+
+vhint_fg="#002269"
+vhint_bg="#6789b4"
+
+vosd_fg=$hint_fg
+vosd_bg=$hint_bg
+
+vlink_fg="#004B91"
+vlink_visited_fg="#680091"
+
+
+export	vwindow_fg	vwindow_bg	vtext_widget_fg	vtext_widget_bg		vselected_fg	vselected_bg		vbutton_fg	vbutton_bg	vbutton_checked_bg	vbutton_checked_fg			vdisabled_fg	vdisabled_bg				vwarningc	verrorc	vsuccessc			vtitlebar_active_fg	vtitlebar_active_bg		vtitlebar_backdrop_fg	vtitlebar_backdrop_bg		vhint_fg	vhint_bg		vosd_fg	vosd_bg		vlink_fg	vlink_visited_fg
+
